0f3c68763e5bda03e1524c747dcf1e6b26f6cf6c
Vitaliy Timoshenko <v_timoshenko@wargaming.net>
2016-02-26 14:08:49 +0200

    new project created

---
+++ Traffic/Traffic/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Traffic
+//
+//  Created by Vitaliy Timoshenko on 2/26/16.
+//  Copyright © 2016 Vitaliy Timoshenko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Traffic/Traffic/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Traffic
+//
+//  Created by Vitaliy Timoshenko on 2/26/16.
+//  Copyright © 2016 Vitaliy Timoshenko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
