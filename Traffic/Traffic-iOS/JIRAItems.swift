68c33a777ac8907e4bce8d560d2a0831b7a952de
Vitaliy Tim <post.vitaliy@gmail.com>
2016-04-30 15:09:25 +0300

    more compact JSON parsing

--- Traffic/Traffic-iOS/JIRAItems.swift
+++ Traffic/Traffic-iOS/JIRAItems.swift
@@ -62,38 +62,28 @@ class JIRATasks {
         guard let items = jsonObjectRoot["issues"] as? Array<AnyObject> else {
             return nil
         }
+        
         for item in items {
-            guard let itemDict = item as? Dictionary<String,AnyObject> else {
-                continue
+            if let itemDict = item as? Dictionary<String,AnyObject> {
+                if let issue_key = itemDict["key"] as? String,
+                       issue_fields_Dict = itemDict["fields"] as? Dictionary<String,AnyObject> {
+                    if let issue_priority_Dict = issue_fields_Dict["priority"] as? Dictionary<String,AnyObject>,
+                           issue_status_Dict = issue_fields_Dict["status"] as? Dictionary<String,AnyObject>,
+                           issue_summary = issue_fields_Dict["summary"] as? String {
+                        let issue_description = issue_fields_Dict["description"] as? String
+                            // issue_description can acutally be empty
+                        if let issue_priority = issue_priority_Dict["name"] as? String,
+                               issue_status = issue_status_Dict["name"] as? String {
+                            
+                            newTasks.append(Task(task_key: issue_key ?? "(no title)",
+                                                task_summary: issue_summary,
+                                                task_priority: issue_priority,
+                                                task_description: issue_description,
+                                                task_status: issue_status))
+                        }
+                    }
+                }
             }
-            guard let issue_key = itemDict["key"] as? String else {
-                continue
-            }
-            guard let issue_fields_Dict = itemDict["fields"] as? Dictionary<String,AnyObject> else {
-                continue
-            }
-            guard let issue_priority_Dict = issue_fields_Dict["priority"] as? Dictionary<String,AnyObject> else {
-                continue
-            }
-            guard let issue_status_Dict = issue_fields_Dict["status"] as? Dictionary<String,AnyObject> else {
-                continue
-            }
-            let issue_description = issue_fields_Dict["description"] as? String // issue_description is allowed to be nil
-            
-            guard let issue_priority = issue_priority_Dict["name"] as? String else {
-                continue
-            }
-            guard let issue_status = issue_status_Dict["name"] as? String else {
-                continue
-            }
-            guard let issue_summary = issue_fields_Dict["summary"] as? String else {
-                continue
-            }
-            newTasks.append(Task(task_key: issue_key ?? "(no title)",
-                                task_summary: issue_summary,
-                                task_priority: issue_priority,
-                                task_description: issue_description,
-                                task_status: issue_status))
         }
         self.init(tasks: newTasks)
     }
@@ -122,10 +112,9 @@ class JIRAerrors {
             return nil
         }
         for item in items {
-            guard let message = item as? String else {
-                continue
+            if let message = item as? String {
+                newErrors.append(Error(error_code: response_code ,error_message: message))
             }
-            newErrors.append(Error(error_code: response_code ,error_message: message))
         }
         self.init(errors: newErrors)
     }
@@ -155,55 +144,32 @@ class JIRATransitions {
         for item in items {
             var the_req_fields = [aReqiredField]()
             
-            guard let itemDict = item as? Dictionary<String,AnyObject> else {
-                continue
-            }
-            guard let transition_id = itemDict["id"] as? String else {
-                continue
-            }
-            guard let transition_name = itemDict["name"] as? String else {
-                continue
-            }
-            guard let transition_toDict = itemDict["to"] as? Dictionary<String,AnyObject> else {
-                continue
-            }
-            guard let transition_target = transition_toDict["name"] as? String else {
-                continue
-            }
-            guard let transition_fields = itemDict["fields"] as? Dictionary<String,AnyObject> else {
-                continue
-            }
-            for (_, item) in transition_fields {
-                if let required_field = item["required"] as? Bool where required_field == true {
-                    
-                    guard let fieldDict = item as? Dictionary<String,AnyObject> else {
-                        continue
-                    }
-                    
-                    guard let name = fieldDict["name"] as? String else {
-                        continue
-                    }
-                    
-                    guard let allowedValues = fieldDict["allowedValues"] as? [Dictionary<String, AnyObject>] else {
-                        continue
-                    }
+            if let itemDict = item as? Dictionary<String,AnyObject> {
+                if let transition_id = itemDict["id"] as? String,
+                    let transition_name = itemDict["name"] as? String,
+                    let transition_fields = itemDict["fields"] as? Dictionary<String,AnyObject>,
+                    let transition_toDict = itemDict["to"] as? Dictionary<String,AnyObject> {
 
-                    guard let operations = fieldDict["operations"] as? Array<String> else {
-                        continue
+                    if let transition_target = transition_toDict["name"] as? String {
+                        for (_, item) in transition_fields {
+                            if let required_field = item["required"] as? Bool where required_field == true {
+                                if let fieldDict = item as? Dictionary<String,AnyObject> {
+                                    if let name = fieldDict["name"] as? String,
+                                        let allowedValues = fieldDict["allowedValues"] as? [Dictionary<String, AnyObject>],
+                                        let operations = fieldDict["operations"] as? Array<String>,
+                                        let schemaDict = fieldDict["schema"] as? Dictionary<String,String>,
+                                        let type = schemaDict["type"] {
+                                        
+                                        the_req_fields.append(aReqiredField(allowedValues: allowedValues, operations: operations, name: name, type: type))
+                                    }
+                                }
+                            }
+                        }
+                        newTransitions.append(Transition(transition_id: transition_id, transition_name: transition_name, target_status: transition_target, required_fields: the_req_fields))
+
                     }
-                    
-                    guard let schemaDict = fieldDict["schema"] as? Dictionary<String,String> else {
-                        continue
-                    }
-                    guard let type = schemaDict["type"] else {
-                        continue
-                    }
-                    
-                    the_req_fields.append(aReqiredField(allowedValues: allowedValues, operations: operations, name: name, type: type))
                 }
             }
-            
-            newTransitions.append(Transition(transition_id: transition_id, transition_name: transition_name, target_status: transition_target, required_fields: the_req_fields))
         }
         self.init(transitions: newTransitions)
     }
